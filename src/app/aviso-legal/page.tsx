6ae6f2fb213e5323dc679d38f70a0a522c157336
Mario González <mario@mariogl.com>
2024-08-30 13:43:01 +0200

    Add legal info page

---
+++ src/app/aviso-legal/page.tsx
@@ -0,0 +1,420 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Aviso legal - The Refactor Project",
+  description: "Aviso legal",
+};
+
+const LegalPage = (): React.ReactElement => {
+  return (
+    <main className="main-content">
+      <div className="container">
+        <section className="section section--legal">
+          <h3 className="section__title">Aviso legal</h3>
+          <p>&nbsp;</p>
+
+          <h4>1.- INFORMACIÓN LEGAL Y ACEPTACIÓN</h4>
+
+          <p>
+            En cumplimiento con lo establecido en la Ley 34/2002 de 11 de Julio,
+            de Servicios de la Sociedad de la Información y el Comercio
+            Electrónico (LSSICE), las presentes disposiciones regulan el uso del
+            servicio del Website https://the-refactor-project.com, (en adelante
+            &quot;Website&quot;) que The Refactor Project S.L.U. pone a
+            disposición de los usuarios de Internet.
+          </p>
+          <p>
+            El acceso al Website es gratuito salvo en lo relativo al coste de la
+            conexión a través de la red de telecomunicaciones suministrada por
+            el proveedor de acceso contratado por los usuarios. Determinados
+            servicios son exclusivos para nuestros clientes y su acceso se
+            encuentra restringido. Los servicios prestados por The Refactor
+            Project S.L.U. con carácter general tienen el precio (IVA no
+            incluido) que se indique en la correspondiente propuesta de
+            servicios y en ningún caso se ofrecen o prestan a través del
+            servicio del portal.
+          </p>
+          <p>
+            En aquellos casos y secciones que nuestro Website ofrezca servicios
+            de Comercio Electrónico sujeto a contratación o contraprestación
+            económica, se deberán aceptar las Condiciones Generales de
+            Contratación especificadas en su caso.
+          </p>
+          <p>
+            La utilización del Website atribuye la condición de usuario del
+            Website (en adelante, Usuario) e implica la aceptación de todas las
+            condiciones incluidas en este Aviso Legal. La prestación del
+            servicio del Website tiene una duración limitada al momento en que
+            el Usuario se encuentre conectado al Website o a alguno de los
+            servicios que a través del mismos se facilitan. Por tanto, el
+            Usuario debe leer atentamente el presente Aviso Legal en cada una de
+            las ocasiones en que se proponga utilizar el Website, ya que éste y
+            sus condiciones de uso recogidas en el presente Aviso Legal pudieran
+            sufrir modificaciones.
+          </p>
+          <p>
+            Algunos servicios del Website accesibles para los usuarios de
+            Internet o exclusivos para los clientes de The Refactor Project
+            S.L.U. pueden estar sometidos a condiciones particulares, reglamento
+            e instrucciones que, en su caso, sustituyen, completan y/o modifican
+            el presente Aviso Legal y que deberán ser aceptadas por el Usuario
+            antes de iniciarse la prestación de los servicios correspondientes.
+          </p>
+
+          <h4>2.- IDENTIFICACIÓN DEL PROPIETARIO DEL WEBSITE</h4>
+          <table>
+            <tbody>
+              <tr>
+                <td style={{ width: 200 }}>Titular del Website:</td>
+                <td>The Refactor Project S.L.U. - CIF B56946866</td>
+              </tr>
+              <tr>
+                <td>Dirección:</td>
+                <td>
+                  Rambla del Brasil n.º 10, 2º 3ª. 8028, Barcelona (Barcelona),
+                  España
+                </td>
+              </tr>
+              <tr>
+                <td>Contacto:</td>
+                <td>
+                  Tel. 680774846 - Email:{" "}
+                  <a href="mailto:info@the-refactor-project.com">
+                    info@the-refactor-project.com
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Registro mercantil o autorización administrativa:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Actividad:</td>
+                <td>Formación en programación</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <h4>3.- PROPIEDAD INTELECTUAL, INDUSTRIAL Y FRAMES</h4>
+
+          <p>
+            Todos los elementos que forman el Website, entendiendo por estos a
+            título meramente enunciativo, los textos, fotografías, gráficos,
+            imágenes, iconos, tecnología, softwares, links y demás contenidos
+            audiovisuales o sonoros, así como su estructura, diseño gráfico,
+            códigos fuente, así como los logos, marcas y demás signos
+            distintivos que aparecen en el mismo, son titularidad de The
+            Refactor Project S.L.U. o de sus colaboradores y están protegidos
+            por los correspondientes derechos de propiedad intelectual e
+            industrial.
+          </p>
+          <p>
+            El titular del sitio Web (The Refactor Project S.L.U) prohíbe
+            expresamente la realización de &quot;framing&quot; o la utilización
+            por parte del Usuario o de terceros de cualesquiera otros mecanismos
+            que alteren el diseño, configuración original o contenidos de
+            nuestro sitio Web.
+          </p>
+          <p>
+            El uso de los contenidos deberá respetar su licenciamiento
+            particular. De tal modo su uso, reproducción, distribución,
+            comunicación pública, transformación o cualquier otra actividad
+            similar o análoga, queda totalmente prohibida salvo que medie previa
+            y expresa autorización del titular del sitio Web, The Refactor
+            Project S.L.U.
+          </p>
+          <p>
+            Únicamente se autoriza la reproducción total o parcial de los textos
+            y contenidos proporcionados por el sitio Web, siempre que concurran
+            todas y cada una de las siguientes condiciones:
+          </p>
+          <ul>
+            <li>
+              Se mantenga la integridad de los contenidos, documentos o gráficos
+            </li>
+            <li>
+              Se cite expresamente al titular del sitio Web como fuente y origen
+              de aquellos.
+            </li>
+            <li>
+              El propósito y la finalidad de tal uso sea compatible con los
+              fines de la Web y/o actividad del titular del sitio Web.
+            </li>
+            <li>
+              No se pretenda un uso comercial, quedando expresamente prohibidas
+              su distribución, comunicación pública, transformación o
+              descompilación.
+            </li>
+          </ul>
+          <p>
+            Cualquier otro uso habrá de ser comunicado y autorizado por el
+            titular del sitio Web, previa y expresamente.
+          </p>
+          <p>
+            Respecto a las citas de productos y servicios de terceros, The
+            Refactor Project S.L.U. reconoce a favor de sus titulares los
+            correspondientes derechos de propiedad industrial e intelectual, no
+            implicando su sola mención o aparición en la Web la existencia de
+            derechos ni de responsabilidad alguna sobre los mismos, como tampoco
+            respaldo, patrocinio o recomendación.
+          </p>
+          <p>
+            The Refactor Project S.L.U. declara su respeto a los derechos de
+            propiedad intelectual e industrial de terceros; por ello, si
+            considera que el Website pudieran estar violando sus derechos,
+            rogamos se ponga en contacto con The Refactor Project S.L.U.
+          </p>
+
+          <h4>4.- CONDICIONES GENERALES DE USO</h4>
+
+          <p>
+            El Website tiene como objeto facilitar al público, en general, el
+            conocimiento de las actividades que esta organización realiza y de
+            los productos y servicios que presta, para el desarrollo de su
+            actividad.
+          </p>
+          <p>
+            El uso de cualquiera de las funcionalidades del sitio Web implica la
+            expresa y plena aceptación de las condiciones aquí expuestas, sin
+            perjuicio de aquellas particulares que pudieran aplicarse a algunos
+            de los servicios concretos ofrecidos a través del sitio Web.
+          </p>
+          <p>
+            El titular del sitio Web se reserva la facultad de efectuar, en
+            cualquier momento y sin necesidad de previo aviso, modificaciones y
+            actualizaciones de la información contenida en su Web o en la
+            configuración y presentación de ésta.
+          </p>
+          <p>
+            Con el objeto de mantener actualizada la información publicada el
+            portal Web, los contenidos del mismo podrán ser modificados,
+            corregidos, eliminados o añadidos en cualquier momento, por lo que
+            será conveniente comprobar la vigencia o exactitud de los mismos
+            acudiendo a las fuentes oficiales.
+          </p>
+
+          <h4>5.- EXCLUSIÓN DE RESPONSABILIDAD</h4>
+
+          <h5>5.1.- DE LA INFORMACIÓN</h5>
+
+          <p>
+            El acceso al Website no implica obligación por parte de The Refactor
+            Project S.L.U. de comprobar la veracidad, exactitud, adecuación,
+            idoneidad, exhaustividad y actualidad de la información suministrada
+            a través del mismo. Los contenidos de este Website son de carácter
+            general y no constituyen, en modo alguno, la prestación de un
+            servicio de asesoramiento legal o fiscal de ningún tipo, por lo que
+            dicha información resulta insuficiente para la toma de decisiones
+            personales o empresariales por parte del Usuario.
+          </p>
+          <p>
+            The Refactor Project S.L.U. no se responsabiliza de las decisiones
+            tomadas a partir de la información suministrada en el Website ni de
+            los datos y perjuicios producidos en el Usuario o terceros con
+            motivo de actuaciones que tengan como único fundamento la
+            información obtenida en el Website.
+          </p>
+          <p>
+            La información está sometida a posibles cambios periódicos sin
+            previo aviso de su contenido por ampliación, mejora, corrección o
+            actualización de los contenidos del Website.
+          </p>
+
+          <h5>5.2.- DE LA CALIDAD DEL SERVICIO DEL WEBSITE</h5>
+
+          <p>
+            El acceso al Website no implica la obligación por parte de The
+            Refactor Project S.L.U. de controlar la ausencia de virus, gusanos o
+            cualquier otro elemento informático dañino. Corresponde al Usuario,
+            en todo caso, la disponibilidad de herramientas adecuadas para la
+            detección y desinfección de programas informáticos dañinos.
+          </p>
+          <p>
+            The Refactor Project S.L.U. no se responsabiliza de los daños
+            producidos en los equipos informáticos de los Usuarios o de terceros
+            durante la prestación del servicio del Website, ni del mal
+            funcionamiento del navegador Web o el uso de versiones no
+            actualizadas del mismo.
+          </p>
+
+          <h5>5.3.- DE LA DISPONIBILIDAD DEL SERVICIO</h5>
+
+          <p>
+            El acceso al Website requiere de servicios y suministros de
+            terceros, incluidos el transporte a través de redes de
+            telecomunicaciones cuya fiabilidad, calidad, continuidad y
+            funcionamiento no corresponde a The Refactor Project S.L.U. Por
+            consiguiente, los servicios que se proveen a través del Website
+            pueden ser suspendidos, cancelados o resultar inaccesibles, con
+            carácter previo o simultaneo a la prestación del servicio del
+            Website.
+          </p>
+          <p>
+            The Refactor Project S.L.U. no se responsabiliza de los daños y
+            perjuicios de cualquier tipo producidos en el Usuario que traigan
+            causa de fallos o desconexiones en las redes de telecomunicaciones
+            que produzcan la suspensión, cancelación o interrupción del servicio
+            del Website durante la prestación del mismo o con carácter previo.
+          </p>
+
+          <h5>
+            5.4.- DE LOS CONTENIDOS Y SERVICIOS ENLAZADOS A TRAVÉS DEL PORTAL
+          </h5>
+
+          <p>
+            El servicio de acceso al Website incluye dispositivos técnicos de
+            enlace, directorios e incluso instrumentos de búsqueda que permiten
+            al Usuario acceder a otras páginas y portales de Internet (en
+            adelante, &quot;Sitios Enlazados&quot;). En estos casos, The
+            Refactor Project S.L.U actúa como prestador de servicios de
+            intermediación de conformidad con el artículo 17 de la Ley 34/2002,
+            de 12 de julio, de Servicios de la Sociedad de la Información y el
+            Comercio Electrónico (LSSICE) y sólo será responsable de los
+            contenidos y servicios suministrados en los Sitios Enlazados en la
+            medida en que tenga conocimiento efectivo de la ilicitud y no haya
+            desactivado el enlace con la debida diligencia.
+          </p>
+
+          <p>
+            En el supuesto de que el Usuario considere que existe un Sitio
+            Enlazado con contenidos ilícitos o inadecuados podrá comunicárselo a
+            The Refactor Project S.L.U de acuerdo con el procedimiento y los
+            efectos establecidos en la cláusula 7, sin que en ningún caso esta
+            comunicación conlleve la obligación de retirar el correspondiente
+            enlace.
+          </p>
+
+          <p>
+            En ningún caso, la existencia de Sitios Enlazados debe presuponer la
+            existencia de acuerdos con los responsables o titulares de los
+            mismos, ni la recomendación, promoción o identificación de The
+            Refactor Project S.L.U con las manifestaciones, contenidos o
+            servicios que se proveen.
+          </p>
+
+          <p>
+            The Refactor Project S.L.U no conoce los contenidos y servicios de
+            los Sitios Enlazados y por tanto no se hace responsable por los
+            daños producidos por la ilicitud, calidad, desactualización,
+            indisponibilidad, error e inutilidad de los contenidos y/o servicios
+            de los Sitios Enlazados ni por cualquier otro daño que no sea
+            directamente imputable a The Refactor Project S.L.U
+          </p>
+
+          <h4>6.- PROTECCIÓN DE DATOS PERSONALES</h4>
+
+          <p>
+            Para aquellos casos que se recaben, traten o almacenen datos
+            personales, se hará en conformidad con la Política de Privacidad
+            publicada en el Website y que puede consultar en el pie o footer del
+            sitio Web.
+          </p>
+
+          <h4>7.- USO DE COOKIES</h4>
+
+          <p>
+            El Website utiliza cookies conforme a lo establecido en nuestra
+            Política de Cookies que puede consultar en el pie o footer del sitio
+            Web.
+          </p>
+
+          <h4>
+            8.- COMUNICACIÓN DE ACTIVIDADES DE CARÁCTER ILÍCITO O INADECUADO
+          </h4>
+
+          <p>
+            En el caso de que el Usuario o cualquier otro usuario de Internet
+            tuviera conocimiento de que los Sitios Enlazados remiten a páginas
+            cuyos contenidos o servicios son ilícitos, nocivos, denigrantes,
+            violentos o contrarios a la moral podrá ponerse en contacto con The
+            Refactor Project S.L.U indicando los siguientes extremos:
+          </p>
+          <ul>
+            <li>
+              Datos personales del comunicante: nombre, dirección, número de
+              teléfono y dirección de correo electrónico.
+            </li>
+            <li>
+              Descripción de los hechos que revelan el carácter ilícito o
+              inadecuado del Sitio Enlazado.
+            </li>
+            <li>
+              En el supuesto de violación de derechos, tales como propiedad
+              intelectual e industrial, los datos personales del titular del
+              derecho infringido cuando sea persona distinta del comunicante.
+              Asimismo, deberá aportar el título que acredite la legitimación
+              del titular de los derechos y, en su caso, el de representación
+              para actuar por cuenta del titular cuando sea persona distinta del
+              comunicante.
+            </li>
+            <li>
+              Declaración expresa de que la información contenida en la
+              reclamación es exacta.
+            </li>
+          </ul>
+          <p>
+            La recepción por parte de The Refactor Project S.L.U de la
+            comunicación prevista en esta cláusula no supondrá, según lo
+            dispuesto en la LSSI, el conocimiento efectivo de las actividades
+            y/o contenidos indicados por el comunicante.
+          </p>
+          <p>
+            Asimismo, deberá aportar el título que acredite la legitimación del
+            titular de los derechos y, en su caso, el de representación para
+            actuar por cuenta del titular cuando sea persona distinta del
+            comunicante.
+          </p>
+
+          <h4>9.- MODIFICACIÓN DEL AVISO LEGAL</h4>
+
+          <p>
+            Con el fin de mejorar las prestaciones del sitio Web, The Refactor
+            Project S.L.U se reserva la facultad de efectuar, en cualquier
+            momento y sin necesidad de previo aviso, modificaciones y
+            actualizaciones de la información contenida en el sitio Web, de la
+            configuración y diseño de éste y del presente aviso legal, así como
+            de cualesquiera otras condiciones particulares. Por tanto, El
+            Usuario debe leer el Aviso Legal en todas y cada una de las
+            ocasiones en que acceda al Website.
+          </p>
+
+          <h4>10.- JURISDICCIÓN</h4>
+
+          <p>
+            The Refactor Project S.L.U se reserva la facultad de presentar las
+            acciones civiles o penales que considere necesarias por la
+            utilización indebida del Sitio Web y Contenidos, o por el
+            incumplimiento de las presentes condiciones del Aviso Legal.
+          </p>
+
+          <p>
+            Para cuantas cuestiones se susciten sobre la interpretación,
+            aplicación y cumplimiento de este Aviso Legal, así como de las
+            reclamaciones que puedan derivarse de su uso, todas las partes
+            intervinientes se someterán a la jurisdicción ordinaria sometiéndose
+            a los Jueces y Tribunales que correspondan conforme a derecho.
+          </p>
+
+          <h4>11.- LEY APLICABLE</h4>
+
+          <p>
+            La ley aplicable en caso de disputa o conflicto de interpretación de
+            los términos que conforman este Aviso Legal, así como cualquier
+            cuestión relacionada con los servicios del presente sitio Web, se
+            regirá por la normativa vigente y de aplicación en el territorio
+            español.
+          </p>
+
+          <p>
+            Reservados todos los derechos de autor por las leyes y tratados
+            internacionales de propiedad intelectual. Queda expresamente
+            prohibida su copia, reproducción o difusión, total o parcial, por
+            cualquier medio.
+          </p>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default LegalPage;
