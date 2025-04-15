7df4a4b82eca90d5c4cc2b2b34993265b3fa7637
Mario <mario@mariogl.com>
2025-04-15 16:54:48 +0200

    Change may to september

--- src/components/CraftingCourse/CraftingCourse.tsx
+++ src/components/CraftingCourse/CraftingCourse.tsx
@@ -35,7 +35,7 @@ const CraftingCourse = (): React.ReactElement => {
           <strong>empresas de programación</strong> en un/a junior.
         </p>
         <p className="section__subtitle">
-          <strong>Plazas disponibles</strong> para la edición de mayo.
+          <strong>Plazas disponibles</strong> para la edición de septiembre.
         </p>
         <p className="section__subtitle">
           También tienes la opción de no pagar nada y empezar a pagar a plazos{" "}
