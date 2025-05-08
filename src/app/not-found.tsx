5293cbefa183972652ccc724ca1540ce5baa084e
Mario <mario@mariogl.com>
2025-05-08 11:22:56 +0200

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <main className="main-content">
+      <div className="container">
+        <section className="section section--centered" style={{ height: 500 }}>
+          <h1>404 - Página no encontrada</h1>
+          <p>Lo sentimos, esta página no existe 🤷‍♀️.</p>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default NotFound;
